65ad685f7489355efa83d0edeed6f5832683c17a
Ian Kimani <ianmwng@gmail.com>
2023-10-03 13:50:47 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+
+function submitData(userName, UserEmail) {
+    const userData = {
+        name: userName,
+        email: UserEmail,
+    };
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    })
+    .then(res => {
+        if (!res.ok) {
+            throw new Error('Unauthorized Access');
+        }
+        return res.json();
+    })
+    .then(data => {
+        const newId = data.id;
+        appendIdToDom(newId);
+    })
+    .catch(error => {
+        appendErrorToDom(error.message);
+    });
+}
+
+function appendIdToDom(newId) {
+    const idContainer = document.createElement('p');
+    idContainer.textContent = `new ID : ${newId}`;
+    document.body.appendChild(idContainer);
+}
+
+function appendErrorToDom(errorMessage) {
+    const errorContainer = document.createElement('p');
+    errorContainer.textContent = `Error: ${errorMessage}`;
+    document.body.appendChild(errorContainer);
+}
